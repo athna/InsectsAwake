99f629fe3c8351a4767dc5c50b27005cfd85d520
jeffzhang <zhangjunfeng01@weidian.com>
2019-02-20 20:58:39 +0800

    2019.02.20 delete

--- InsectsAwake/views/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author  : jeffzhang
-# @Time    : 2018/04/03
-# @File    : __init__.py.py
-# @Desc    : ""
--- InsectsAwake/views/modules/week_passwd/week_http_passwd_test.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# @Author  : jeffzhang
-# @Time    : 2018/04/20
-# @File    : week_http_passwd_test.py
-# @Desc    : ""
-
-
-
